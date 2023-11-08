3d540f5b1190ba9f71ad486f25dd65394cae0cd1
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2023-11-08 22:14:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,17 @@
 function Person(name, age) {
 	this.name = name;
     this.age = age;
+	this.greet = function () {
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+    };
 }
 
 function Employee(name, age, jobTitle) {
 	Person.call(this, name, age);
     this.jobTitle = jobTitle;
+	this.jobGreet = function () {
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+    };
 }
 
 // Do not change code below this line
